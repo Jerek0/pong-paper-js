70c0c55fbb9942bc0eda315b552d815df5606d0b
Jerek0 <jeremy.minie@gmail.com>
2014-11-03 18:07:40 +0100

    Added scoring support + Reorganized code

--- js/main.js
+++ js/main.js
@@ -1,15 +1,25 @@
 var cpt;
 
-// ####### CLASSE GAME
+// ############################# //
+// ####### CLASSE GAME ######### //
+// ############################# //
+
 var Game = function() {
 	this.players = [];
+	this.ui = new PointText(new Point(window.innerWidth/2-20,20));
+	this.ui.fillColor = "black";
 };
 
 Game.prototype.init = function() {
 	this.bouboule = new Pallet();
 	this.players[0] = new Player(new Point(50, 100), ['a','q']);
 	this.players[1] = new Player(new Point(window.innerWidth-100, 100), ['p','l']);
+	this.updateScores();
 };
+
+Game.prototype.updateScores = function() {
+	this.ui.content = this.players[0].score+' - '+this.players[1].score;
+}
 
 Game.prototype.checkCollisions = function() {
 	for(cpt=0; cpt<this.players.length; cpt++) {
@@ -18,14 +28,23 @@ Game.prototype.checkCollisions = function() {
 	}
 };
 
-// ####### CLASSE PLAYER
+Game.prototype.controlsManager = function() {
+	for(cpt=0;cpt<this.players.length; cpt++) {
+		if(this.players[cpt].keyStatus[0]) this.players[cpt].move(-this.players[cpt].inerty);
+		if(this.players[cpt].keyStatus[1]) this.players[cpt].move(+this.players[cpt].inerty);
+	}
+}
+
+// ############################# //
+// ####### CLASSE PLAYER ####### //
+// ############################# //
+
 var Player = function(pos, keyMap) {
 	var _self = this;
-	this.keyMap = keyMap;
+	this.keyMap = keyMap; // 0 -> UP / 1 -> DOWN
 	this.keyStatus = [false,false];
 	this.inerty = 10;
-
-	console.log(this.keyMap[0]);
+	this.score = 0;
 
 	this.view = new Path.Rectangle({
 		point : pos, 
@@ -38,15 +57,22 @@ Player.prototype.move = function(delta) {
 	this.view.position.y += delta;
 }
 
-// ####### CLASSE PALLET
-var Pallet = function() {
-	this.vitesse = {x: 6*(Math.round(Math.random())*2-1), y : 6*(Math.round(Math.random())*2-1)};
+// ############################# //
+// ####### CLASSE PALET ######## //
+// ############################# //
 
+var Pallet = function() {
+	this.reset();
+}
+
+Pallet.prototype.reset = function() {
+	if(this.view != null) this.view.remove();
 	this.view = new Path.Circle({
 		center: view.center,
 		radius: 20,
 		strokeColor: 'black'
 	});
+	this.vitesse = {x: 6*(Math.round(Math.random())*2-1), y : 6*(Math.round(Math.random())*2-1)};
 }
 
 Pallet.prototype.move = function() {
@@ -55,18 +81,48 @@ Pallet.prototype.move = function() {
 }
 
 Pallet.prototype.checkFrameCollisions = function() {
-	if(this.view.position.x >= window.innerWidth || this.view.position.x <= 0) {
-		this.vitesse.x = -this.vitesse.x;
-	}
 	if(this.view.position.y >= window.innerHeight || this.view.position.y <= 0) {
 		this.vitesse.y = -this.vitesse.y;
 	}
-	
-
 }
+
+Pallet.prototype.checkWins = function(players) {
+	if(this.view.position.x >= window.innerWidth || this.view.position.x <= 0) {
+		if(this.view.position.x > window.innerWidth) players[0].score++;
+		if(this.view.position.x < 0) players[1].score++;
+
+		return true;
+	}
+}
+
+// =================================================================================== //
+
+// ###################### //
+// ####### INIT ######### //
+// ###################### //
 
 var game = new Game();
 game.init();
+
+// ############################# //
+// ####### MAIN LOOP ########### //
+// ############################# //
+
+function onFrame(event) {
+	game.controlsManager();
+	game.bouboule.move();
+	game.checkCollisions();
+	game.bouboule.checkFrameCollisions();
+	
+	if(game.bouboule.checkWins(game.players)) {
+		game.bouboule.reset();
+		game.updateScores();	
+	} 
+}
+
+// ############################# //
+// ####### KEY LISTENERS ####### //
+// ############################# //
 
 function onKeyDown(event) {
 	switch(event.key) {
@@ -114,14 +170,4 @@ function onKeyUp(event) {
 		game.players[1].keyStatus[1]=false;
 		break;
 	}
-}
-
-function onFrame(event) {
-	for(cpt=0;cpt<game.players.length; cpt++) {
-		if(game.players[cpt].keyStatus[0]) game.players[cpt].move(-game.players[cpt].inerty);
-		if(game.players[cpt].keyStatus[1]) game.players[cpt].move(+game.players[cpt].inerty);
-	}
-	game.checkCollisions();
-	game.bouboule.checkFrameCollisions();
-	game.bouboule.move();
 }
